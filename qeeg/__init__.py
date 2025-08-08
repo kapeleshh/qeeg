6129c99b28b5b41dd1c87d02aad1989c55669c31
kapeleshh <kapeleshh@gmail.com>
2025-08-08 14:59:45 +0900

    Fix circular import issues in module initialization

--- qeeg/__init__.py
+++ qeeg/__init__.py
@@ -11,15 +11,6 @@ This package provides tools for EEG data analysis, including:
 
 __version__ = '0.1.0'
 
-# Import submodules
-from . import preprocessing
-from . import analysis
-from . import visualization
-from . import ml
-from . import utils
-from . import conditions
-from . import cli
-
 # Define __all__ to control what is imported with "from qeeg import *"
 __all__ = [
     'preprocessing',
@@ -30,3 +21,12 @@ __all__ = [
     'conditions',
     'cli',
 ]
+
+# Import submodules - these are at the end to avoid circular imports
+from . import preprocessing
+from . import utils
+from . import analysis
+from . import visualization
+from . import conditions
+from . import cli
+from . import ml
--- qeeg/ml/__init__.py
+++ qeeg/ml/__init__.py
@@ -7,10 +7,13 @@ This module provides functions for applying machine learning to EEG data, includ
 - Model evaluation
 """
 
-from . import features
-from . import classification
-
+# Define __all__ to control what is imported with "from qeeg.ml import *"
 __all__ = [
     'features',
     'classification',
 ]
+
+# Avoid circular imports by not importing submodules here
+# Users will need to import them explicitly:
+# from qeeg.ml import features
+# from qeeg.ml import classification
